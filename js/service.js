2fec066b25fadc140aa2cb5458eb85fc13544f63
mummabear80 <133831722+mummabear80@users.noreply.github.com>
2023-05-17 04:23:21 +0100

    Delete js directory

--- js/service.js
+++
@@ -1,9 +0,0 @@
-burger = document.querySelector('.burger')
-navList = document.querySelector('.nav-list')
-navbar = document.querySelector('.navbar')
-rightNav =  document.querySelector('.rightNav')
-burger.addEventListener('click', ()=>{
-    navbar.classList.toggle('h-nav-resp');
-    navList.classList.toggle('v-class-resp');
-    rightNav.classList.toggle('v-class-resp');
-})
